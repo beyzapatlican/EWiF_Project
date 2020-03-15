7f04271ceb5255694428af5615d27648a3debdef
Beyza <beyza786@outlook.com>
2020-03-15 22:13:14 +0300

    New

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
+import {FooterComponent} from './footer/footer.component';
+import {LehrerComponent} from './lehrer/lehrer.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
+import {SignInComponent} from './sign-in/sign-in.component';
+import {StudentComponent} from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForgotPasswordComponent,
+    FooterComponent,
+    LehrerComponent,
+    NavbarComponent,
+    SignUpComponent,
+    SignInComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
