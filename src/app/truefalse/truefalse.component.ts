2553addc8a158df144a4c51401b01b5b571ee7ef
merve sari <mervesarinin@gmail.com>
2020-04-04 20:23:36 +0300

    new

--- src/app/truefalse/truefalse.component.ts
+++ src/app/truefalse/truefalse.component.ts
@@ -6,33 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./truefalse.component.css']
 })
 export class TruefalseComponent implements OnInit {
-  public question = '';
-  public questionId = '';
-  public deleteId = '';
-  public answer = '';
-  public answer2 = '';
-  public answer3 = '';
-  public answer4 = '';
-  public arr = [];
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
-  ekle(question: string, questionId: string ) {
-    const ekleme = {question, questionId};
-    this.arr.push(ekleme);
-  }
 
-  sil(deleteId: string) {
-    this.arr = this.arr.filter(value => !(value.questionId === deleteId));
-  }
-
-  check(forEachfunc) {
-    let bulundu = false;
-    this.arr.forEach(value =>  {if ( this.questionId === value.questionId) {
-      bulundu = true;
-    } });
-    return bulundu;
-  }
 
 }
