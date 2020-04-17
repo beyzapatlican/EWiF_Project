f1fce575024961bbf0f799eea0f1d003f53f7e60
Alp A <38848833+Umptor@users.noreply.github.com>
2020-04-17 23:08:03 +0300

    create html links to variables and functions for session submission

--- src/app/freetext/freetext.component.ts
+++ src/app/freetext/freetext.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {saveQuestion} from '../question-type/question-type.component';
+import {Free} from '../models/question-types/free.model';
+
 
 @Component({
   selector: 'app-freetext',
   templateUrl: './freetext.component.html',
   styleUrls: ['./freetext.component.css']
 })
+
+
+
 export class FreetextComponent implements OnInit {
+  question: string;
+  solution: string;
+  questionNum: number;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+
+  saveQuestion(question: string, solution: string, questionNum: number) {
+    const newQuestion = new Free(question, solution, questionNum);
+    saveQuestion(undefined, newQuestion, undefined);
   }
-
-
 }
