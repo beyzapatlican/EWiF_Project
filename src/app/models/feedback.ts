0a5cb528c70aa635556df16a8b378d496e096736
Alp A <38848833+Umptor@users.noreply.github.com>
2020-05-04 19:08:16 +0300

    move feedback stuff into feedback.ts

--- src/app/models/feedback.ts
+++ src/app/models/feedback.ts
@@ -4,8 +4,19 @@ export class Feedback {
   openSessionName: string;
   openSessionPin: string;
 
+  feedbackValues: Array<string> = ['Schlecht', 'Durchschnittlich', 'Gut'];
+  feedbackQuestions: Array<string> = [
+    'Allgemeine Zufriedenheit: ',
+    'Gesamterlebnis:',
+    'Schwierigkeit der Lektion:' ,
+    'Geschwindigkeit der Lektion:' ,
+    'Verständlichkeit der Lektion:',
+    'Konnte der Dozent Ihre Frage beantworten?: ',
+    'War der Inhalt interessant?: ' ];
+  counters: Array<number> = Array(0, 1, 2, 3, 4, 5, 6);
 
-  constructor(answers: string, lectureName: string, openSessionName: string, openSessionPin: string, counters: number) {
+
+  constructor(answers: string, lectureName?: string, openSessionName?: string, openSessionPin?: string) {
     this.answers = answers;
     this.lectureName = lectureName;
     this.openSessionName = openSessionName;
