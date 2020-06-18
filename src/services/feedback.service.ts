759e1360705700c91b5baaa480bc8a6953c82fa5
Alp A <38848833+Umptor@users.noreply.github.com>
2020-06-18 22:15:00 +0300

    Condense token and url retrieval in feedback.service.ts

--- src/services/feedback.service.ts
+++ src/services/feedback.service.ts
@@ -4,6 +4,8 @@ import {environment} from '../environments/environment.prod';
 import {Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 import {Router} from '@angular/router';
+import {TokenService} from './token.service';
+import {UrlService} from './url.service';
 
 class Resp {
   body: string;
@@ -20,20 +22,21 @@ class Resp {
   providedIn: 'root'
 })
 export class FeedbackService {
-  private readonly apiUrl = 'http://24.133.107.44:8080';
   private givenFeedback: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   get giveFeedback() {
     return this.givenFeedback.asObservable();
   }
   constructor(private http: HttpClient,
+              private tokenService: TokenService,
+              private urlService: UrlService,
               private router: Router) {}
 
   Give(obj) {
     const headers = {
-      Authorization: this.getToken(),
+      Authorization: this.tokenService.getToken(),
       'Content-Type': 'application/json'};
-    return this.http.post(this.getUrl() + '/' + 'giveFeedback', obj, {observe: 'response'});
+    return this.http.post(this.urlService.getURL() + '/' + 'giveFeedback', obj, {observe: 'response'});
   }
 
   done(object) {
@@ -42,14 +45,5 @@ export class FeedbackService {
       this.givenFeedback.next(true);
       this.router.navigate(['/']);
     }
-  }
-
-  private getToken(): string {
-    // tslint:disable-next-line:max-line-length
-    return 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX3JvbGUiOiJBRE1JTiIsInN1YiI6ImFscCIsImlzcyI6Ik1hbHBpc20iLCJpYXQiOjE1ODQzOTE5NjJ9.cjzdW7kOZZzG_K8cgYj33BIh8KukQyoCaZiMhO517_U';
-  }
-
-  private getUrl(): string {
-    return environment.urlAddress;
   }
 }
