706f8eec912b5bcb9cf8f625d7db5e4f471afee8
Alp A <38848833+Umptor@users.noreply.github.com>
2020-06-18 22:15:47 +0300

    Remove unnecessary code in feedback.service.ts

--- src/services/feedback.service.ts
+++ src/services/feedback.service.ts
@@ -1,22 +1,10 @@
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {environment} from '../environments/environment.prod';
 
 import {Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 import {Router} from '@angular/router';
 import {TokenService} from './token.service';
 import {UrlService} from './url.service';
-
-class Resp {
-  body: string;
-  headers: HttpHeaders;
-
-
-  constructor(body: string,  headers: HttpHeaders) {
-    this.body = body;
-    this.headers = headers;
-  }
-}
 
 @Injectable({
   providedIn: 'root'
@@ -37,13 +25,5 @@ export class FeedbackService {
       Authorization: this.tokenService.getToken(),
       'Content-Type': 'application/json'};
     return this.http.post(this.urlService.getURL() + '/' + 'giveFeedback', obj, {observe: 'response'});
-  }
-
-  done(object) {
-    if (object.a !== '' && object.b !== '' && object.c !== '' && object.d !== '' && object.e !== ''
-      && object.f !== '' && object.g !== '' && object.pin !== '') {
-      this.givenFeedback.next(true);
-      this.router.navigate(['/']);
-    }
   }
 }
