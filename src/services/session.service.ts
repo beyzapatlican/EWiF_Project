b0fe79b3f9c931bfb3be7774803be24da6a6bd16
Alp A <38848833+Umptor@users.noreply.github.com>
2020-06-19 10:14:17 +0300

    Reformat session.service.ts

--- src/services/session.service.ts
+++ src/services/session.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { UrlService} from './url.service';
+import {UrlService} from './url.service';
 import {DeleteSessionRequest} from '../models/requests/deleteSessionRequest.model';
 import {DeleteSessionResponse} from '../models/responses/deleteSessionResponse.model';
 import {AnfangenRequest} from '../models/requests/anfangen-request.model';
@@ -32,25 +32,26 @@ export class SessionService {
   }
 
   userCount(pinOpen: string) {
-    return this.http.get<OpenSessionUserCountResponse>(this.urlService.getURL() + '/teacher/openSessionUserCount', {params: { pinOpen }});
+    return this.http.get<OpenSessionUserCountResponse>(this.urlService.getURL() + '/teacher/openSessionUserCount', {params: {pinOpen}});
   }
 
   anfangen() {
     this.router.navigate(['/createSession']);
-    }
+  }
 
-    basla(pin: string) {
+  basla(pin: string) {
     const request = new AnfangenRequest(pin);
     return this.http.post<AnfangenResponse>(`${this.urlService.getURL()}/teacher/createSession`, request);
   }
 
   startExam(OpenPin: string) {
-      const request = new StartExamRequest(OpenPin);
-      return this.http.post(`${this.urlService.getURL()}/teacher/startExam`, request);
-    }
-    start() {
-      this.router.navigate(['/session-management']);
-    }
+    const request = new StartExamRequest(OpenPin);
+    return this.http.post(`${this.urlService.getURL()}/teacher/startExam`, request);
+  }
+
+  start() {
+    this.router.navigate(['/session-management']);
+  }
 
   skip(pinOpen: string) {
     const request = new SkipRequest(pinOpen);
@@ -71,7 +72,7 @@ export class SessionService {
   }
 
   getAnswer(pinOpen: string) {
-    return this.http.get<GetAnswerResponse>(`${this.urlService.getURL()}/teacher/questionAnswer`, { params: { pinOpen }});
+    return this.http.get<GetAnswerResponse>(`${this.urlService.getURL()}/teacher/questionAnswer`, {params: {pinOpen}});
   }
 
 }
