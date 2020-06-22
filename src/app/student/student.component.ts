62a81f2a72a3c4e3ae5265aa5e6b5450d6b8a293
Beyza <beyza786@outlook.com>
2020-06-22 21:56:39 +0300

    student main page

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -8,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class StudentComponent implements OnInit {
 
   constructor() { }
-
+  selected1 = true;
+  selected2 = false;
   ngOnInit() {
+  }
+  onUpdate1() {
+    this.selected1 = false;
+    this.selected2 = true;
+  }
+
+  onUpdate2() {
+    this.selected2 = true;
+    this.selected1 = false;
+  }
+  refresh(): void {
+    window.location.reload();
   }
 }
