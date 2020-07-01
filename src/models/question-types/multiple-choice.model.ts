ab23873033eff5fe6df2f5851db492adc9a5376a
Alp A <38848833+Umptor@users.noreply.github.com>
2020-07-01 12:36:01 +0300

    Fix incorrect multiple-choice.model constructor

--- src/models/question-types/multiple-choice.model.ts
+++ src/models/question-types/multiple-choice.model.ts
@@ -1,24 +1,24 @@
 export class MultipleChoice {
-  question: string;
-  solution: number;
-  ans1: string;
-  ans2: string;
-  ans3: string;
-  ans4: string;
-  ans5: string;
-  questionNum: number;
+    question: string;
+    solution: number;
+    ans1: string;
+    ans2: string;
+    ans3: string;
+    ans4: string;
+    ans5: string;
+    questionNum: number;
 
-  constructor(question: string, solution?: number,
-              ans1?: string, ans2?: string, ans3?: string, ans4?: string, ans5?,
-              questionNum?: number) {
+    constructor(question: string, solution?: number,
+                ans1?: string, ans2?: string, ans3?: string, ans4?: string, ans5?,
+                questionNum?: number) {
 
-    this.question = question;
-    this.solution = solution;
-    this.questionNum = questionNum;
-    this.ans1 = ans1;
-    this.ans2 = ans2;
-    this.ans3 = ans3;
-    this.ans4 = ans4;
-    this.ans5 = ans5;
-  }
+        this.question = question;
+        this.solution = solution;
+        this.questionNum = questionNum;
+        this.ans1 = ans1;
+        this.ans2 = ans2;
+        this.ans3 = ans3;
+        this.ans4 = ans4;
+        this.ans5 = ans5;
+    }
 }
